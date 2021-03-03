6c31a94763e7a9e04f68a21d482decff4c1d81da
@lukeocodes <luke@lukeoliff.com>
2021-03-03 21:43:34 +0000

    feat(scenes): pet is MOVING OMGZZZZZzzzz

--- src/components/Pet.vue
+++ src/components/Pet.vue
@@ -2,8 +2,10 @@
   <div
     id="pet"
     ref="pet"
-    :class="status"
-    class="absolute bottom-0 right-0 w-8 h-8 border-2 border-black"
+    :class="state"
+    class="absolute w-8 h-8 bg-blue-200 border-2 border-black"
+    :style="styleObj"
+    @click="getNewRight"
   ></div>
 </template>
 
@@ -11,16 +13,63 @@
 export default {
   data() {
     return {
-      status: "idle",
+      state: "idle",
       right: 0,
-      newRight: 0
+      newRight: 0,
+      timeToMove: 0,
+      pxToMove: 0
     };
   },
 
-  methods: {
-    walkToRight: () => {},
+  computed: {
+    styleObj() {
+      return {
+        zIndex: "1000",
+        right: `${this.right}px`,
+        bottom: 0,
+        transition: `right ${this.timeToMove}s linear`
+      };
+    },
 
-    getNewRight: () => {}
+    maxRight() {
+      return window.innerWidth - this.petSize.width;
+    },
+
+    maxBottom() {
+      return window.innerHeight - this.petSize.height;
+    },
+
+    petSize() {
+      return this.$refs.pet.getBoundingClientRect();
+    }
+  },
+
+  watch: {
+    newRight(newRight, oldRight) {
+      this.$nextTick(() => {
+        this.pxToMove = Math.abs(oldRight - newRight);
+        this.timeToMove = this.pxToMove / 50;
+        this.state = `walking ${oldRight > newRight ? "left" : "right"}`;
+        this.right = newRight;
+      });
+    }
+  },
+
+  mounted() {
+    // this.maxRight = this.$refs.app.offsetWidth;
+
+    // this.newRight = this.getNewRight();
+
+    this.getNewRight();
+  },
+
+  methods: {
+    walkToRight() {},
+
+    getNewRight() {
+      console.log("yes?");
+      this.newRight = Math.floor(Math.random() * (this.maxRight - 1 + 1) + 1);
+    }
   }
 };
 </script>
