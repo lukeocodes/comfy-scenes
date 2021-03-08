43725c1a7c1ad574cc82777d870fda224b269826
@lukeocodes <luke@lukeoliff.com>
2021-03-08 17:00:04 +0000

    feat(scenes): change shadow to GLOW MORE

--- src/components/StreamTitle.vue
+++ src/components/StreamTitle.vue
@@ -39,23 +39,24 @@ export default {
 
 h1 {
   font-family: "Beon";
-  font-size: 5rem;
+  font-size: 6rem;
   color: #ffe;
 
   text-shadow: 0 0 2px rgba(255, 255, 255, 0.5),
     0 0 4px rgba(255, 255, 255, 0.75), 0 0 6px rgba(255, 255, 255, 0.25),
     0 0 8px #228dff, 0 0 10px #228dff, 0 0 12px #228dff, 0 0 14px #228dff,
     0 0 16px #228dff, 0 0 32px #228dff, 0 0 64px #228dff, 0 0 128px #228dff,
-    0px 6px 3px #000000;
+    0 0 128px #228dff, 0 0 128px #228dff, 0 0 128px #228dff,
+    0px 12px 3px #000000;
   animation: blink linear infinite 200s;
 }
 
 h2 {
   font-family: "Beon";
-  font-size: 2rem;
+  font-size: 3rem;
   color: #ffd1bf;
-  text-shadow: 0 -40px 100px, 0 0 2px, 0 0 1em #ff4444, 0 0 0.5em #ff4444,
-    0 0 0.1em #ff8f44, 0 6px 3px #000000;
+  text-shadow: 0 -40px 100px, 0 0 2px, 0 0 1em #ff4444, 0 0 2em #ff4444,
+    0 0 3em #ff4444, 0 0 0.5em #ff4444, 0 0 0.1em #ff8f44, 0 12px 3px #000000;
 }
 
 div {
@@ -78,7 +79,8 @@ div {
       0 0 4px rgba(255, 255, 255, 0.75), 0 0 6px rgba(255, 255, 255, 0.25),
       0 0 8px #228dff, 0 0 10px #228dff, 0 0 12px #228dff, 0 0 14px #228dff,
       0 0 16px #228dff, 0 0 32px #228dff, 0 0 64px #228dff, 0 0 128px #228dff,
-      0px 6px 3px #000000;
+      0 0 128px #228dff, 0 0 128px #228dff, 0 0 128px #228dff,
+      0px 12px 3px #000000;
   }
   2.79% {
     color: rgb(97, 97, 97);
@@ -92,7 +94,8 @@ div {
       0 0 4px rgba(255, 255, 255, 0.75), 0 0 6px rgba(255, 255, 255, 0.25),
       0 0 8px #228dff, 0 0 10px #228dff, 0 0 12px #228dff, 0 0 14px #228dff,
       0 0 16px #228dff, 0 0 32px #228dff, 0 0 64px #228dff, 0 0 128px #228dff,
-      0px 6px 3px #000000;
+      0 0 128px #228dff, 0 0 128px #228dff, 0 0 128px #228dff,
+      0px 12px 3px #000000;
   }
   2.87% {
     color: rgb(97, 97, 97);
@@ -104,7 +107,8 @@ div {
       0 0 4px rgba(255, 255, 255, 0.75), 0 0 6px rgba(255, 255, 255, 0.25),
       0 0 8px #228dff, 0 0 10px #228dff, 0 0 12px #228dff, 0 0 14px #228dff,
       0 0 16px #228dff, 0 0 32px #228dff, 0 0 64px #228dff, 0 0 128px #228dff,
-      0px 6px 3px #000000;
+      0 0 128px #228dff, 0 0 128px #228dff, 0 0 128px #228dff,
+      0px 12px 3px #000000;
   }
   90.90% {
     color: #ffe;
@@ -112,7 +116,8 @@ div {
       0 0 4px rgba(255, 255, 255, 0.75), 0 0 6px rgba(255, 255, 255, 0.25),
       0 0 8px #228dff, 0 0 10px #228dff, 0 0 12px #228dff, 0 0 14px #228dff,
       0 0 16px #228dff, 0 0 32px #228dff, 0 0 64px #228dff, 0 0 128px #228dff,
-      0px 6px 3px #000000;
+      0 0 128px #228dff, 0 0 128px #228dff, 0 0 128px #228dff,
+      0px 12px 3px #000000;
   }
   90.92% {
     color: rgb(97, 97, 97);
@@ -124,7 +129,8 @@ div {
       0 0 4px rgba(255, 255, 255, 0.75), 0 0 6px rgba(255, 255, 255, 0.25),
       0 0 8px #228dff, 0 0 10px #228dff, 0 0 12px #228dff, 0 0 14px #228dff,
       0 0 16px #228dff, 0 0 32px #228dff, 0 0 64px #228dff, 0 0 128px #228dff,
-      0px 6px 3px #000000;
+      0 0 128px #228dff, 0 0 128px #228dff, 0 0 128px #228dff,
+      0px 12px 3px #000000;
   }
 }
 </style>
